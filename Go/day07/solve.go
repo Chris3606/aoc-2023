12f7395f31823f405991a4c806575d5756147cdd
Chris3606 <cridley106@gmail.com>
2023-12-07 09:38:03 -0600

    Cleaned up day 7.

--- Go/day07/solve.go
+++ Go/day07/solve.go
@@ -23,7 +23,7 @@ const (
 	FiveOfAKind  = 7
 )
 
-var cardVals = map[byte]int{
+var cardValuesPart1 = map[byte]int{
 	'2': 2,
 	'3': 3,
 	'4': 4,
@@ -39,7 +39,7 @@ var cardVals = map[byte]int{
 	'A': 14,
 }
 
-var cardVals2 = map[byte]int{
+var cardValuesPart2 = map[byte]int{
 	'J': 1,
 	'2': 2,
 	'3': 3,
@@ -89,162 +89,108 @@ func parseInput(path string) ([]Hand, error) {
 	return hands, nil
 }
 
-func (hand Hand) getStrength() int {
+func (hand Hand) getStrength(replaceJokers bool) int {
 	hist := utils.BuildHistogram(hand.Hand)
 
 	item, maxCount := utils.MaxFromMap(hist)
 
-	switch maxCount {
-	case 5:
-		return FiveOfAKind
-	case 4:
-		return FourOfAKind
-	case 3:
-		hist[item] = 0
-
-		_, newMax := utils.MaxFromMap(hist)
-		if newMax == 2 {
-			return FullHouse
-		}
-
-		// 3 of a kind
-		return ThreeOfAKind
-	case 2:
-		hist[item] = 0
-		_, secondMax := utils.MaxFromMap(hist)
-
-		// Two pair
-		if secondMax == 2 {
-			return TwoPair
-		}
-
-		// One pair
-		return OnePair
-	case 1:
-		return HighCard
-	default:
-		panic("Bad hand.")
-	}
-}
-
-func (hand Hand) getStrength2() int {
-	hist := utils.BuildHistogram(hand.Hand)
-
-	item, maxCount := utils.MaxFromMap(hist)
-	if maxCount != 5 {
+	// The best thing to do with a joker (if we're using them) is simply to add it to whatever the
+	// largest number of cards is; more of a card is always better than anything you can do with
+	// an equal number of a given card
+	if replaceJokers && maxCount != 5 {
 		if item == 'J' {
+			// If jokers was the max, we'll dump them into whatever is the second highest
 			hist[item] = 0
 			nextItem, _ := utils.MaxFromMap(hist)
 			hist[nextItem] += maxCount
 		} else {
+			// Otherwise, just jump the jokers into the highest card
 			hist[item] += hist['J']
 		}
 
+		// Re-calculate the max with jokers (and remove the jokers from the histogram)
 		hist['J'] = 0
 		item, maxCount = utils.MaxFromMap(hist)
 	}
 
+	// The max count of cards will tell the strength
 	switch maxCount {
 	case 5:
 		return FiveOfAKind
 	case 4:
 		return FourOfAKind
 	case 3:
+		// Check next highest; if there is 3 + 2, we have full house
 		hist[item] = 0
-
 		_, newMax := utils.MaxFromMap(hist)
 		if newMax == 2 {
 			return FullHouse
 		}
 
-		// 3 of a kind
+		// Otherwise, 3 of a kind
 		return ThreeOfAKind
 	case 2:
+		// Check next highest; if there is 2 + 2, we have two pair
 		hist[item] = 0
 		_, secondMax := utils.MaxFromMap(hist)
-
-		// Two pair
 		if secondMax == 2 {
 			return TwoPair
 		}
 
-		// One pair
+		// Otherwise, one pair
 		return OnePair
-	case 1:
+	case 1: // Just a high card
 		return HighCard
 	default:
 		panic("Bad hand.")
 	}
 }
 
-func compareHands(h1, h2 Hand) int {
-	h1Str := h1.getStrength()
-	h2Str := h2.getStrength()
-	diff := h1Str - h2Str
+func getHandComparer(strengthFunc func(Hand) int, cardVals map[byte]int) func(Hand, Hand) int {
+	return func(h1, h2 Hand) int {
+		h1Str := strengthFunc(h1)
+		h2Str := strengthFunc(h2)
+		diff := h1Str - h2Str
 
-	if diff != 0 {
-		return diff
-	}
-
-	for i := 0; i < len(h1.Hand); i++ {
-		i1 := cardVals[h1.Hand[i]]
-		i2 := cardVals[h2.Hand[i]]
-		if i1 > i2 {
-			return 1
-		} else if i1 < i2 {
-			return -1
+		if diff != 0 {
+			return diff
 		}
-	}
 
-	return 0
+		for i := 0; i < len(h1.Hand); i++ {
+			i1 := cardVals[h1.Hand[i]]
+			i2 := cardVals[h2.Hand[i]]
+			if i1 > i2 {
+				return 1
+			} else if i1 < i2 {
+				return -1
+			}
+		}
+
+		return 0
+	}
 }
 
-func compareHands2(h1, h2 Hand) int {
-	h1Str := h1.getStrength2()
-	h2Str := h2.getStrength2()
-	diff := h1Str - h2Str
+func calculateTotalWinnings(hands []Hand, replaceJokers bool, cardVals map[byte]int) int {
+	slices.SortFunc(hands, getHandComparer(func(h Hand) int { return h.getStrength(replaceJokers) }, cardVals))
 
-	if diff != 0 {
-		return diff
+	winnings := 0
+	for i, h := range hands {
+		winnings += (i + 1) * h.Bid
 	}
 
-	for i := 0; i < len(h1.Hand); i++ {
-		i1 := cardVals2[h1.Hand[i]]
-		i2 := cardVals2[h2.Hand[i]]
-		if i1 > i2 {
-			return 1
-		} else if i1 < i2 {
-			return -1
-		}
-	}
-
-	return 0
+	return winnings
 }
 
 func PartA(path string) int {
 	hands, err := parseInput(path)
 	utils.CheckError(err)
 
-	slices.SortFunc(hands, compareHands)
-
-	winnings := 0
-	for i, h := range hands {
-		winnings += (i + 1) * h.Bid
-	}
-
-	return winnings
+	return calculateTotalWinnings(hands, false, cardValuesPart1)
 }
 
 func PartB(path string) int {
 	hands, err := parseInput(path)
 	utils.CheckError(err)
 
-	slices.SortFunc(hands, compareHands2)
-
-	winnings := 0
-	for i, h := range hands {
-		winnings += (i + 1) * h.Bid
-	}
-
-	return winnings
+	return calculateTotalWinnings(hands, true, cardValuesPart2)
 }
