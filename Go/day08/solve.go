43c8a3be72e3c144f465903b1bfe91ba56675a90
Chris3606 <cridley106@gmail.com>
2023-12-08 10:46:15 -0600

    Finished day 8 in Go.

--- Go/day08/solve.go
+++ Go/day08/solve.go
@@ -6,33 +6,93 @@ import (
 	"os"
 )
 
-func parseInput(path string) ([]string, error) {
+type NodeData struct {
+	Left  string
+	Right string
+}
+
+func parseInput(path string) (string, map[string]NodeData, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
-	for scanner.Scan() {
-		// Parse lines
+	directions, err := utils.ReadStringFromScanner(scanner)
+	if err != nil {
+		return "", nil, err
 	}
 
-	return []string{"not", "implemented"}, nil
+	// Skip blank line
+	_, err = utils.ReadStringFromScanner(scanner)
+	if err != nil {
+		return "", nil, err
+	}
+
+	nodes := map[string]NodeData{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		nodes[text[:3]] = NodeData{text[7:10], text[12:15]}
+	}
+
+	return directions, nodes, nil
 }
 
-func PartA(path string) string {
-	_, err := parseInput(path)
-	utils.CheckError(err)
+func followPath(nodes map[string]NodeData, curNode string, dir byte) string {
+	if dir == 'L' {
+		curNode = nodes[curNode].Left
+	} else {
+		curNode = nodes[curNode].Right
+	}
 
-	return "Not implemented"
+	return curNode
 }
 
-func PartB(path string) string {
-	_, err := parseInput(path)
+func PartA(path string) int {
+	directions, nodes, err := parseInput(path)
 	utils.CheckError(err)
 
-	return "Not implemented"
+	curNode := "AAA"
+	curDirIdx := 0
+	steps := 0
+
+	for curNode != "ZZZ" {
+		curDirection := directions[curDirIdx]
+		curNode = followPath(nodes, curNode, curDirection)
+
+		steps++
+		curDirIdx = (curDirIdx + 1) % len(directions)
+	}
+
+	return steps
+}
+
+func PartB(path string) int {
+	directions, nodes, err := parseInput(path)
+	utils.CheckError(err)
+
+	var paths []int
+	for k := range nodes {
+		if k[2] != 'A' {
+			continue
+		}
+
+		curNode := k
+		curDirIdx := 0
+		steps := 0
+		for curNode[2] != 'Z' {
+			curDirection := directions[curDirIdx]
+			curNode = followPath(nodes, curNode, curDirection)
+
+			steps++
+			curDirIdx = (curDirIdx + 1) % len(directions)
+		}
+
+		paths = append(paths, steps)
+	}
+
+	return utils.LCM(paths[0], paths[1], paths[2:]...)
 }
