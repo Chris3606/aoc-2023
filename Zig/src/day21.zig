98c4775a32e3707899a917e190de7d0a140d02bf
Chris3606 <cridley106@gmail.com>
2023-12-08 21:02:09 -0600

    Added Zig template.

---
+++ Zig/src/day21.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+const util = @import("util.zig");
+const gpa = util.gpa;
+
+const data = @embedFile("data/day21.txt");
+
+pub fn main() !void {
+    defer {
+        const deinit_status = util.gpa_impl.deinit();
+        if (deinit_status == .leak) @panic("Leaked memory!");
+    }
+}
+
+// Generated from template/template.zig.
+// Run `zig build generate` to update.
+// Only unmodified days will be updated.
