eadcc7c22c8a7a1ebca5fb342a885151d39a5078
Chris3606 <cridley106@gmail.com>
2023-12-12 10:20:37 -0600

    Finished day 12 part 2 in Go.

--- Go/day12/solve.go
+++ Go/day12/solve.go
@@ -13,31 +13,16 @@ type Row struct {
 	Groups  []int
 }
 
-func (row *Row) GroupsMatch() bool {
-	groups := findGroupsForSprings(row.Springs)
-	return utils.CompareSlicesElementwise(groups, row.Groups)
-}
-
-func findGroupsForSprings(springs []byte) []int {
-	var groups []int
-
-	curGroup := 0
-	for _, s := range springs {
-		if s == '#' {
-			curGroup += 1
-		} else {
-			if curGroup != 0 {
-				groups = append(groups, curGroup)
-				curGroup = 0
-			}
-		}
-	}
-
-	if curGroup != 0 {
-		groups = append(groups, curGroup)
-	}
-
-	return groups
+// Memoizable object.  Since these are slices that point to the same memory
+// throughout the lifetime of recursion, recording addresses is equivalent to
+// recording the objects themselves.
+//
+// However, we DO change values in the unprocessed springs slice as we go along,
+// so our memo needs to include the value too.
+type FuncParams struct {
+	UnprocessedSprings *byte
+	CurSpring          byte
+	CurGroups          *int
 }
 
 func parseInput(path string) ([]Row, error) {
@@ -73,40 +58,7 @@ func parseInput(path string) ([]Row, error) {
 	return rows, nil
 }
 
-// Fill question with . or #, then recurse on next
-// func doTest(row *Row, questions []int, curQuestionIdx int) int {
-// 	matches := 0
-// 	row.Springs[questions[curQuestionIdx]] = '.'
-// 	curGroups := findGroupsForSprings(row.Springs[0 : questions[curQuestionIdx]+1])
-// 	if len(row.Groups) >= len(curGroups) && utils.CompareSlicesElementwise(curGroups, row.Groups[:len(curGroups)]) {
-// 		if curQuestionIdx == len(questions)-1 {
-// 			if row.GroupsMatch() {
-// 				matches++
-// 			}
-// 		} else {
-// 			matches += doTest(row, questions, curQuestionIdx+1)
-// 		}
-// 	}
-
-// 	row.Springs[questions[curQuestionIdx]] = '#'
-// 	curGroups = findGroupsForSprings(row.Springs[0 : questions[curQuestionIdx]+1])
-// 	curGroups = curGroups[:len(curGroups)-1] // Cut off last group since we might still add to it later
-// 	if len(row.Groups) >= len(curGroups) && utils.CompareSlicesElementwise(curGroups, row.Groups[:len(curGroups)]) {
-// 		if curQuestionIdx == len(questions)-1 {
-// 			if row.GroupsMatch() {
-// 				matches++
-// 			}
-// 		} else {
-// 			matches += doTest(row, questions, curQuestionIdx+1)
-// 		}
-// 	}
-
-// 	row.Springs[questions[curQuestionIdx]] = '?'
-
-// 	return matches
-// }
-
-func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
+func countSpringPermutations(unprocessed_springs []byte, cur_groups []int, memo map[FuncParams]int) int {
 	// Base case: no more springs; so if we've satisfied all the groups, we're good; if there's groups
 	// left, we haven't matched
 	if len(unprocessed_springs) == 0 {
@@ -117,9 +69,27 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 		}
 	}
 
+	// If we've memoized the result, return it
+	var spr *byte
+	var grp *int
+	var curSpr byte
+	if len(unprocessed_springs) > 0 {
+		spr = &unprocessed_springs[0]
+		curSpr = unprocessed_springs[0]
+	}
+	if len(cur_groups) > 0 {
+		grp = &cur_groups[0]
+	}
+	params := FuncParams{spr, curSpr, grp}
+	p, ok := memo[params]
+	if ok {
+		return p
+	}
+
 	// First spring
 	ch := unprocessed_springs[0]
 
+	permutations := 0
 	switch ch {
 	// Working springs don't contribute to groups, so they are irrelevant; we'll skip past them
 	case '.':
@@ -130,25 +100,24 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 			unprocessed_springs = unprocessed_springs[idx:]
 		}
 
-		return countSpringPermutations(unprocessed_springs, cur_groups)
+		permutations = countSpringPermutations(unprocessed_springs, cur_groups, memo)
 
 	// Unknown spring; count permutations with it both ways
 	case '?':
-		permutations := 0
+		//permutations := 0
 		unprocessed_springs[0] = '.'
-		permutations += countSpringPermutations(unprocessed_springs, cur_groups)
+		permutations += countSpringPermutations(unprocessed_springs, cur_groups, memo)
 
 		unprocessed_springs[0] = '#'
-		permutations += countSpringPermutations(unprocessed_springs, cur_groups)
+		permutations += countSpringPermutations(unprocessed_springs, cur_groups, memo)
 
 		unprocessed_springs[0] = '?'
-		return permutations
 
 	// Damaged spring; so see if the current group is possible and recurse if needed
 	case '#':
 		// We found a damaged spring, but there is no group for it to belong to
 		if len(cur_groups) == 0 {
-			return 0
+			break
 		}
 
 		// The current group requires x springs, so including this one there must be at least
@@ -156,7 +125,7 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 		cur_group := cur_groups[0]
 		springs_left := len(unprocessed_springs)
 		if cur_group > springs_left {
-			return 0
+			break
 		}
 
 		// Specifically, of the springs remaining, there must be at least x consecutive that are
@@ -168,7 +137,7 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 			}
 		}
 		if group_found < cur_group { // Too few springs in group
-			return 0
+			break
 		}
 
 		// The spring after the current group (if any), must NOT be broken; otherwise the group is
@@ -176,7 +145,7 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 		unprocessed_springs = unprocessed_springs[cur_group:]
 		if len(unprocessed_springs) > 0 {
 			if unprocessed_springs[0] == '#' {
-				return 0
+				break
 			}
 
 			// The next one _has_ to be considered unbroken; so we can just skip past it
@@ -184,11 +153,14 @@ func countSpringPermutations(unprocessed_springs []byte, cur_groups []int) int {
 		}
 
 		// The current group is satisfied; so recurse on the ones after this group
-		return countSpringPermutations(unprocessed_springs, cur_groups[1:])
+		permutations = countSpringPermutations(unprocessed_springs, cur_groups[1:], memo)
 
 	default:
 		panic("Invalid spring state.")
 	}
+
+	memo[params] = permutations
+	return permutations
 }
 
 func expandRows(rows []Row, factor int) {
@@ -221,9 +193,9 @@ func PartA(path string) int {
 		// 	}
 		// }
 
-		// matches := countSpringPermutations(row.Springs, row.Groups)
+		// matches := countSpringPermutations(row.Springs, row.Groups, map[FuncParams]int{})
 		// fmt.Println(matches)
-		sum += countSpringPermutations(row.Springs, row.Groups)
+		sum += countSpringPermutations(row.Springs, row.Groups, map[FuncParams]int{})
 	}
 
 	return sum
@@ -237,7 +209,7 @@ func PartB(path string) int {
 
 	sum := 0
 	for _, row := range rows {
-		sum += countSpringPermutations(row.Springs, row.Groups)
+		sum += countSpringPermutations(row.Springs, row.Groups, map[FuncParams]int{})
 	}
 
 	return sum
