a5ddcf003c5af3375c4b4ff6df1a56b33d1b3b91
Chris3606 <cridley106@gmail.com>
2023-12-14 06:57:02 -0600

    Finished day 13 part 1 in Go.

--- Go/day14/solve.go
+++ Go/day14/solve.go
@@ -2,37 +2,128 @@ package day14
 
 import (
 	"aoc/utils"
-	"bufio"
 	"os"
 )
 
-func parseInput(path string) ([]string, error) {
+// For this one we'll just parse the input into our grid structure.
+func parseInput(path string) (utils.Grid[byte], error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return utils.Grid[byte]{}, err
 	}
 	defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
+	return utils.ReadGridFromBytes(f, func(b byte, p utils.Point) (byte, error) { return b, nil })
+}
 
-	for scanner.Scan() {
-		// Parse lines
+func rollRock(dish utils.Grid[byte], pos, dir utils.Point) {
+	for {
+		neighbor := pos.Add(dir)
+		if !dish.Contains(neighbor) {
+			break
+		}
+
+		if dish.GetCopy(neighbor) != '.' {
+			break
+		}
+
+		dish.Set(pos, '.')
+		pos = neighbor
+		dish.Set(pos, 'O')
+	}
+}
+
+func rollNorth(dish utils.Grid[byte]) {
+	var posIt = dish.Positions()
+	for posIt.Next() {
+		cur := posIt.Current()
+
+		if dish.GetCopy(cur) != 'O' {
+			continue
+		}
+
+		rollRock(dish, cur, utils.UP)
+	}
+}
+
+func rollSouth(dish utils.Grid[byte]) {
+	for y := dish.Height() - 1; y >= 0; y-- {
+		for x := 0; x < dish.Width(); x++ {
+			cur := utils.Point{X: x, Y: y}
+
+			if dish.GetCopy(cur) != 'O' {
+				continue
+			}
+
+			rollRock(dish, cur, utils.DOWN)
+		}
+	}
+}
+
+func rollWest(dish utils.Grid[byte]) {
+	for x := 0; x < dish.Width(); x++ {
+		for y := 0; y < dish.Height(); y++ {
+			cur := utils.Point{X: x, Y: y}
+
+			if dish.GetCopy(cur) != 'O' {
+				continue
+			}
+
+			rollRock(dish, cur, utils.LEFT)
+		}
+	}
+}
+
+func rollEast(dish utils.Grid[byte]) {
+	for x := dish.Width() - 1; x >= 0; x-- {
+		for y := 0; y < dish.Height(); y++ {
+			cur := utils.Point{X: x, Y: y}
+
+			if dish.GetCopy(cur) != 'O' {
+				continue
+			}
+
+			rollRock(dish, cur, utils.RIGHT)
+		}
+	}
+}
+
+func calculateNorthernSupportLoad(dish utils.Grid[byte]) int {
+	sum := 0
+	posIt := dish.Positions()
+	for posIt.Next() {
+		cur := posIt.Current()
+		if dish.GetCopy(cur) == 'O' {
+			sum += dish.Height() - cur.Y
+		}
 	}
 
-	return []string{"not", "implemented"}, nil
+	return sum
 }
 
-func PartA(path string) string {
-	_, err := parseInput(path)
+func PartA(path string) int {
+	dish, err := parseInput(path)
 	utils.CheckError(err)
 
-	return "Not implemented"
+	// Tilt all rocks north
+	rollNorth(dish)
+
+	// Calculate and return support load
+	return calculateNorthernSupportLoad(dish)
 }
 
-func PartB(path string) string {
-	_, err := parseInput(path)
+func PartB(path string) int {
+	dish, err := parseInput(path)
 	utils.CheckError(err)
 
-	return "Not implemented"
+	// Perform cycles
+	for i := 0; i < 1000000000; i++ {
+		rollNorth(dish)
+		rollWest(dish)
+		rollSouth(dish)
+		rollEast(dish)
+	}
+
+	// Calculate and return support load
+	return calculateNorthernSupportLoad(dish)
 }
