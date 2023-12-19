08bb410f31aa38525696b5f3acc447deb01b4a25
Chris3606 <cridley106@gmail.com>
2023-12-19 08:03:09 -0600

    Finished day 17 part 1.

--- Go/day17/solve.go
+++ Go/day17/solve.go
@@ -2,32 +2,136 @@ package day17
 
 import (
 	"aoc/utils"
-	"bufio"
+	"math"
 	"os"
+
+	"github.com/oleiade/lane/v2"
 )
 
-func parseInput(path string) ([]string, error) {
+type State struct {
+	Position  utils.Point
+	Direction utils.Point
+	Travel    int
+}
+
+type AStarItem struct {
+	State State
+	F     int
+	G     int
+}
+
+func (item *AStarItem) Priority() int {
+	return item.F
+}
+
+var neighbors = map[utils.Point][]utils.Point{
+	{X: 0, Y: 0}: {utils.RIGHT, utils.DOWN},
+	utils.UP:     {utils.LEFT, utils.UP, utils.RIGHT},
+	utils.RIGHT:  {utils.UP, utils.RIGHT, utils.DOWN},
+	utils.DOWN:   {utils.RIGHT, utils.DOWN, utils.LEFT},
+	utils.LEFT:   {utils.DOWN, utils.LEFT, utils.UP},
+}
+
+func shortestPath(grid utils.Grid[int], start, end utils.Point) (int, bool) {
+	heap := lane.NewMinPriorityQueue[AStarItem, int]()
+
+	initState := State{Position: start, Direction: utils.RIGHT, Travel: 1}
+	initState2 := State{Position: start, Direction: utils.DOWN, Travel: 1}
+	dist := map[State]int{initState: 0, initState2: 0}
+
+	itm := AStarItem{State: initState, G: 0, F: utils.ManhattanDistance(start, end)}
+	heap.Push(itm, itm.Priority())
+
+	itm = AStarItem{State: initState2, G: 0, F: utils.ManhattanDistance(start, end)}
+	heap.Push(itm, itm.Priority())
+
+	for !heap.Empty() {
+		// Pop item
+		item, _, ok := heap.Pop()
+		if !ok {
+			panic("Remove from priority queue failed.")
+		}
+
+		// We found the shortest path
+		if item.State.Position == end {
+			return item.G, true
+		}
+
+		// If we've already found a better way, we won't visit this node on the current path;
+		// this can happen if multiple states with the same value were pushed into the queue
+		d, ok := dist[item.State]
+		if !ok {
+			d = math.MaxInt
+		}
+		if item.G > d {
+			continue
+		}
+
+		// Test all neighbors to see if there is a better path to them by going though the current
+		// position
+		for _, dir := range neighbors[item.State.Direction] {
+			neighbor := item.State.Position.Add(dir)
+			// Off edge of grid
+			if !grid.Contains(neighbor) {
+				continue
+			}
+
+			// Figure out if we turned or not and set straight line travel dist accordingly.
+			// The starting point (only G == 0 since we don't have negative weights) will count as a
+			// straight line no matter where we move
+			straightLineTravel := item.State.Travel + 1
+			if item.State.Direction != dir {
+				straightLineTravel = 1
+			}
+
+			// Too long in a straight line
+			if straightLineTravel > 3 {
+				continue
+			}
+
+			node := AStarItem{State: State{Position: neighbor, Direction: dir, Travel: straightLineTravel}, G: item.G + grid.GetCopy(neighbor), F: item.G + grid.GetCopy(neighbor) + utils.ManhattanDistance(neighbor, end)}
+
+			// If cost is lower, add it to the list of nodes to visit and update cost
+			d, ok = dist[node.State]
+			if !ok {
+				d = math.MaxInt
+			}
+			if node.G < d {
+				dist[node.State] = node.G
+				heap.Push(node, node.Priority())
+			}
+		}
+	}
+
+	return -1, false
+}
+
+// Simply parse the input into our grid structure
+func parseInput(path string) (utils.Grid[int], error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return utils.Grid[int]{}, err
 	}
 	defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		// Parse lines
+	grid, err := utils.ReadGridFromBytes(f, func(b byte, p utils.Point) (int, error) { return int(b - '0'), nil })
+	if err != nil {
+		return grid, nil
 	}
 
-	return []string{"not", "implemented"}, nil
+	return grid, nil
 }
 
-func PartA(path string) string {
-	_, err := parseInput(path)
+func PartA(path string) int {
+	grid, err := parseInput(path)
 	utils.CheckError(err)
 
-	return "Not implemented"
+	sp, ok := shortestPath(grid, utils.Point{X: 0, Y: 0}, utils.Point{X: grid.Width() - 1, Y: grid.Height() - 1})
+	if !ok {
+		panic("No shortest path found")
+	}
+
+	return sp
 }
 
 func PartB(path string) string {
