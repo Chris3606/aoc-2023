fadfffb636f4def2a9e66c827e271b971daee241
Chris3606 <cridley106@gmail.com>
2023-12-19 09:59:22 -0600

    Finished day 18.

--- Go/day18/solve.go
+++ Go/day18/solve.go
@@ -3,36 +3,149 @@ package day18
 import (
 	"aoc/utils"
 	"bufio"
+	"errors"
 	"os"
+	"strconv"
 )
 
-func parseInput(path string) ([]string, error) {
+func parseInput(path string) ([]utils.Point, int, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, -1, err
 	}
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
+	vertices := []utils.Point{{X: 0, Y: 0}}
+	outerPoints := 0
 	for scanner.Scan() {
-		// Parse lines
+		parts := utils.NewStringDelimiterScanner(scanner.Text(), " ")
+
+		dir, err := utils.ReadStringFromScanner(parts)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+
+		val, err := utils.ReadItemFromScanner(parts, strconv.Atoi)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+
+		var direction utils.Point
+		switch dir {
+		case "U":
+			direction = utils.UP
+		case "D":
+			direction = utils.DOWN
+		case "L":
+			direction = utils.LEFT
+		case "R":
+			direction = utils.RIGHT
+		default:
+			return vertices, outerPoints, errors.New("invalid direction type")
+		}
+
+		last := vertices[len(vertices)-1]
+		vertices = append(vertices, utils.Point{X: last.X + (direction.X * val), Y: last.Y + (direction.Y * val)})
+		outerPoints += val
 	}
 
-	return []string{"not", "implemented"}, nil
+	return vertices, outerPoints, nil
 }
 
-func PartA(path string) string {
-	_, err := parseInput(path)
-	utils.CheckError(err)
+func parseInput2(path string) ([]utils.Point, int, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, -1, err
+	}
+	defer f.Close()
 
-	return "Not implemented"
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	vertices := []utils.Point{{X: 0, Y: 0}}
+	outerPoints := 0
+	for scanner.Scan() {
+		parts := utils.NewStringDelimiterScanner(scanner.Text(), " ")
+
+		_, err := utils.ReadStringFromScanner(parts)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+
+		_, err = utils.ReadStringFromScanner(parts)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+
+		hex, err := utils.ReadStringFromScanner(parts)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+		hex = hex[1 : len(hex)-1] // Strip parens
+
+		v, err := strconv.ParseInt(hex[1:len(hex)-1], 16, 0)
+		if err != nil {
+			return vertices, outerPoints, err
+		}
+		val := int(v)
+
+		var direction utils.Point
+		switch hex[len(hex)-1] {
+		case '0':
+			direction = utils.RIGHT
+		case '1':
+			direction = utils.DOWN
+		case '2':
+			direction = utils.LEFT
+		case '3':
+			direction = utils.UP
+		default:
+			return vertices, outerPoints, errors.New("invalid direction type")
+		}
+
+		last := vertices[len(vertices)-1]
+		vertices = append(vertices, utils.Point{X: last.X + (direction.X * val), Y: last.Y + (direction.Y * val)})
+		outerPoints += val
+	}
+
+	return vertices, outerPoints, nil
 }
 
-func PartB(path string) string {
-	_, err := parseInput(path)
+func getArea(vertices []utils.Point, boundaryPoints int) int {
+	// Shoelace formula.  This gives us the area of the polygon, but since the integer points are
+	// in the center of each square, we're missing 1/2 sq meter around the edge of the outer points.
+	area := 0
+	for i := 0; i < len(vertices); i++ {
+		prev := i - 1
+		if prev == -1 {
+			prev = len(vertices) - 1
+		}
+		next := (i + 1) % len(vertices)
+
+		area += vertices[i].X * (vertices[prev].Y - vertices[next].Y)
+	}
+
+	area = utils.Abs(area) / 2
+
+	// Pick's theorem area formula (rearranged with some algebra)
+	innerPoints := area - boundaryPoints/2 + 1
+
+	return innerPoints + boundaryPoints
+}
+
+func PartA(path string) int {
+	vertices, boundaryPoints, err := parseInput(path)
 	utils.CheckError(err)
 
-	return "Not implemented"
+	return getArea(vertices, boundaryPoints)
+}
+
+func PartB(path string) int {
+	vertices, boundaryPoints, err := parseInput2(path)
+	utils.CheckError(err)
+
+	return getArea(vertices, boundaryPoints)
 }
--- Go/utils/util.go
+++ Go/utils/util.go
@@ -92,20 +92,20 @@ func GetOppositeDir(dir Point) Point {
 	return DIRS_CLOCKWISE[(idx+4)%len(DIRS_CLOCKWISE)]
 }
 
-// type Rectangle struct {
-// 	MinExtent Point
-// 	MaxExtent Point
-// }
+type Rectangle struct {
+	MinExtent Point
+	MaxExtent Point
+}
 
-// func (rect *Rectangle) ExpandToContain(point Point) Rectangle {
-// 	minX := min(point.X, rect.MinExtent.X)
-// 	minY := min(point.Y, rect.MinExtent.Y)
+func (rect *Rectangle) ExpandToContain(point Point) Rectangle {
+	minX := min(point.X, rect.MinExtent.X)
+	minY := min(point.Y, rect.MinExtent.Y)
 
-// 	maxX := max(point.X, rect.MaxExtent.X)
-// 	maxY := max(point.Y, rect.MaxExtent.Y)
+	maxX := max(point.X, rect.MaxExtent.X)
+	maxY := max(point.Y, rect.MaxExtent.Y)
 
-// 	return Rectangle{MinExtent: Point{X: minX, Y: minY}, MaxExtent: Point{X: maxX, Y: maxY}}
-// }
+	return Rectangle{MinExtent: Point{X: minX, Y: minY}, MaxExtent: Point{X: maxX, Y: maxY}}
+}
 
 // Builds a map of how often each element occurs in the given slice
 func BuildHistogram[T comparable](slice []T) map[T]int {
